d70fd8aedd8146633866968a4c79e144f20745f6
Gabryell Cavalcante <49329614+GabryellCavalcante@users.noreply.github.com>
2021-04-27 10:46:12 -0300

    atulaização

---
+++ js/main.js
@@ -0,0 +1,29 @@
+//var nome = "Gabryell Cavalcante";
+//var n1 = 25;
+//var n2 = 10;
+//var frase = "Japão é o melhor time do mundo";
+//alert(nome + " tem " + idade + " anos");
+//alert (idade +  idade2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase);
+//console.log(frase.replace("Japão","Brasil"));
+//console.log(n1 + n2);
+//console.log(n1 - n2);
+//console.log(n1 * n2);
+//console.log(n1 / n2);
+
+//var lista = ["maçã","pêra","laranja"]
+//lista.push("uva")
+//console.log(lista);
+//lista.pop("uva")
+//console.log(lista);
+
+//console.log(lista.reverse());
+//console.log(lista.length);
+//console.log(lista[0]);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+//var frutas = [{nome:"maça",cor:"vermelho"},{nome:"uva",cor:"roxa"}]
+//console.log(frutas[1].nome);
